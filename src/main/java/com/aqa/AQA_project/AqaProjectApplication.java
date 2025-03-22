0f29021c8cabc90379f913ba12abe62c57f27b2e
Sairku <kukrias@gmail.com>
2025-03-22 23:48:50 +0200

    first commit

---
+++ src/main/java/com/aqa/AQA_project/AqaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aqa.AQA_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aqa/AQA_project/AqaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqa.AQA_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
